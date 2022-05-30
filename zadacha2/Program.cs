45afe625b5de053c72281f46eb2345520a56b3bb
Sunlicious <Sunlicious@mail.ru>
2022-05-30 22:28:16 +0500

    Задача 2. Найти макисмальное из трех

---
+++ zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 2;
+int numberB = 3;
+int numberC = 7;
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
