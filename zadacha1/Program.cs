d7280b04a4255a3ba8eae01edac02e83309689cf
Sunlicious <Sunlicious@mail.ru>
2022-05-30 22:25:33 +0500

    Задача 1. Найти максимальное из двух

---
+++ zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+Console.Write("Введите число: ");
+string writeNumberA = Console.ReadLine();
+int numberA = Convert.ToInt32(writeNumberA);
+
+Console.Write("Введите число: ");
+string writeNumberB = Console.ReadLine();
+int numberB = Convert.ToInt32(writeNumberB);
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+
+Console.Write("max = ");
+Console.WriteLine(max);
