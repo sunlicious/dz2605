bd6944ae448ca2372c4114bd9d33e153cc08f004
Sunlicious <Sunlicious@mail.ru>
2022-05-30 22:33:16 +0500

    Для любого числа

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,6 +1,14 @@
-﻿int numberA = 2;
-int numberB = 3;
-int numberC = 7;
+﻿Console.Write("Введите число: ");
+string writeNumberA = Console.ReadLine();
+int numberA = Convert.ToInt32(writeNumberA);
+
+Console.Write("Введите число: ");
+string writeNumberB = Console.ReadLine();
+int numberB = Convert.ToInt32(writeNumberB);
+
+Console.Write("Введите число: ");
+string writeNumberC = Console.ReadLine();
+int numberC = Convert.ToInt32(writeNumberC); 
 
 int max = numberA;
 
