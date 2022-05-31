1048eb6e1bf5886a376711fa49aa5dfab46fe932
Sunlicious <Sunlicious@mail.ru>
2022-05-31 13:20:12 +0500

    Четное или нечетное

---
+++ zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;                             
+    Console.Write("введите число: ");
+    a= Console.Read();
+     if (a % 2 == 0)
+    {
+    Console.WriteLine("число четное");
+    }
+    else
+    {
+    Console.WriteLine("Число нечетное");
+    }
