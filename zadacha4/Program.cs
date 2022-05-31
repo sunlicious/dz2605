4bf5f4fb49385ef499ed37902024a5e62f054420
Sunlicious <Sunlicious@mail.ru>
2022-05-31 19:00:08 +0500

    Четные числа от 1 до N

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:   ");
+string A = Console.ReadLine();
+
+int endNum = Convert.ToInt32(A);
+int num = 0;
+Console.WriteLine($"Чётные числа из диапазона от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) Console.Write($"{num} ");
+}
+Console.WriteLine();
